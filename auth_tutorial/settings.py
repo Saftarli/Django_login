6cb2d3cd2875eb3fc5741b315ea91a3efecaad86
saftarli <tofiqsefterli@gmail.com>
2025-01-28 00:28:40 +0400

    duzelis

--- auth_tutorial/settings.py
+++ auth_tutorial/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=dw9zg*wk=#2ra*0fz#&hu@t2*_5l0erolvj!&hqw83lsa7%9&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
